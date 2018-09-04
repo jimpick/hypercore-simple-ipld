51570c9456a9b70e47447307083d6cbcf9988e84
Jim Pick <jim@backblend.com>
2018-09-03 18:35:10 -0700

    Write out leaf node cbor wrappers

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const hypercore = require('hypercore')
 const ram = require('random-access-memory')
 const print = require('print-flat-tree')
+const dagPB = require('ipld-dag-pb')
+const ipfsAPI = require('ipfs-api')
+
+const ipfs = new ipfsAPI('/ip4/127.0.0.1/tcp/5001')
+
+const recordDataCids = []
+const nodeCids = []
+const signatureNodeCids = []
 
 const feed = hypercore(ram)
 feed.append('record a', err => {
@@ -43,7 +51,16 @@ function dumpRecord (index, cb) {
   feed.get(index, (err, data) => {
     if (err) return cb(err)
     console.log('Data', index, data.toString())
-    cb()
+    dagPB.DAGNode.create(data, (err, node) => {
+      if (err) return cb(err)
+      const opts = { format: 'dag-pb', hashAlg: 'sha2-256' }
+      ipfs.dag.put(node, opts, (err, cid) => {
+        if (err) return cb(err)
+        console.log('CID:', cid.toBaseEncodedString())
+        recordDataCids[index] = cid
+        cb()
+      })
+    })
   })
 }
 
@@ -57,7 +74,24 @@ function dumpNode (index, cb) {
       } else {
         console.log('Sign', index, signature)
       }
-      cb()
+      if (index % 2 === 0) {
+        // Leaf node
+        const data = {
+          index: index,
+          hash: node.hash,
+          size: node.size,
+          leaf: {'/': recordDataCids[index / 2].toBaseEncodedString()}
+        }
+        const options = { format: 'dag-cbor', hashAlg: 'sha3-512' }
+        ipfs.dag.put(data, options, (err, cid) => {
+          if (err) return cb(err)
+          nodeCids[index] = cid
+          console.log('CID:', cid.toBaseEncodedString())
+          cb()
+        })
+      } else {
+        cb()
+      }
     })
   })
 }
